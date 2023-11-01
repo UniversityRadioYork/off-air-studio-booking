c909d60c3778736670b1fb09fae64f6a1ca14540
michael-grace <michael.dp.grace@outlook.com>
2023-11-01 15:47:08 +0000

    myradiokey in deployment

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 IMAGE="evergiven.ury.york.ac.uk:5000/off-air-studio-booking"
 CONTAINER="off-air-studio-booking"
-ENV="/opt/off-air-studio-booking/.env"
+aPROJECTDIR="/opt/off-air-studio-booking"
 PORT=3090
 
 docker build -t $IMAGE .
 docker push $IMAGE
 docker stop $CONTAINER || echo 0
 docker rm $CONTAINER || echo 0
-docker run -d --env-file $ENV --name $CONTAINER -p $PORT:8080 $IMAGE
+docker run -d --env-file $PROJECTDIR/.env --name $CONTAINER -p $PORT:8080 -v $PROJECTDIR/.myradio.key:/usr/src/app/.myradio.key $IMAGE
