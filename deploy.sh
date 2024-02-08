a3e10ce0dfaecef1270ffbc74d94c5e20faf526f
michael-grace <michael.dp.grace@outlook.com>
2024-02-08 19:05:14 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,8 @@ IMAGE="evergiven.ury.york.ac.uk:5000/off-air-studio-booking"
 CONTAINER="off-air-studio-booking"
 PROJECTDIR="/opt/off-air-studio-booking"
 PORT=3090
+DATE=$(date +%s)
 
-docker build -t $IMAGE .
-docker push $IMAGE
-docker stop $CONTAINER || echo 0
-docker rm $CONTAINER || echo 0
-docker run -d --env-file $PROJECTDIR/.env --name $CONTAINER -p $PORT:8080 -v $PROJECTDIR/.myradio.key:/usr/src/app/.myradio.key $IMAGE
+docker build -t $IMAGE:$DATE .
+docker push $IMAGE:$DATE
+docker service update --image $IMAGE:$DATE off-air-bookings
